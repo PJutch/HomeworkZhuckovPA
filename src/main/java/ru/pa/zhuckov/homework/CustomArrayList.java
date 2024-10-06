773b0ff3ac58b9710d4d9286bb530c3533392b3b
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-06 15:43:47 +0300

    Add CustomArrayList and some stubs

---
+++ src/main/java/ru/pa/zhuckov/homework/CustomArrayList.java
@@ -0,0 +1,20 @@
+package ru.pa.zhuckov.homework;
+
+public class CustomArrayList<T> implements CustomList<T> {
+    private T[] values = (T[]) new Object[0];
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T get(int index) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+
+    }
+}
