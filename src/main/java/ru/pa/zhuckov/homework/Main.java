da2e1baa33a67ab8e0cc33e485539dbc111246e6
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-06 15:53:43 +0300

    Add some examples to main

--- src/main/java/ru/pa/zhuckov/homework/Main.java
+++ src/main/java/ru/pa/zhuckov/homework/Main.java
@@ -2,6 +2,16 @@ package ru.pa.zhuckov.homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        System.out.println(list.get(1));
+
+        System.out.println(list.remove(1));
+        System.out.println(list);
+        System.out.println(list.get(1));
     }
 }
