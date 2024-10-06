24e99af236b3324df5314cd249641c84fe56ce2b
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-06 15:38:10 +0300

    Add javadoc to the interface

--- src/main/java/ru/pa/zhuckov/homework/CustomList.java
+++ src/main/java/ru/pa/zhuckov/homework/CustomList.java
@@ -1,7 +1,26 @@
 package ru.pa.zhuckov.homework;
 
 public interface CustomList<T> {
+    /**
+     * Добавляет элемент в конец
+     *
+     * @param value значение нового элемента
+     */
     void add(T value);
+
+    /**
+     * Получает значение элемента с данным индексом
+     *
+     * @param index индекс этого элемента, индексация с 0
+     * @return значение элемента с индексом index
+     */
     T get(int index);
+
+    /**
+     * Удаляет элемент с данным индексом
+     *
+     * @param index индекс этого элемента, индексация с 0
+     * @return значение удалённого элемента
+     */
     T remove(int index);
 }
